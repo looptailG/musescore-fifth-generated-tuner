249cbedd8f5c7c6919b18e294538bb0858f489a7
Alessandro Culatti <99362337+looptailG@users.noreply.github.com>
2026-05-17 16:28:18 +0200

    Updated year

--- source/StringUtils.js
+++ source/StringUtils.js
@@ -1,6 +1,6 @@
 /*
 	A collection of functions for manipulating strings.
-	Copyright (C) 2024 Alessandro Culatti
+	Copyright (C) 2024, 2026 Alessandro Culatti
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
